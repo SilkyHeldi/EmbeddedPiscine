991e72dd21db7dbc3e6b7b29e5efd3c965eec1fc
llepiney <llepiney@student.42.fr>
2024-04-18 13:31:58 +0200

    D03 Z is printed

---
+++ D03/ex00/main.c
@@ -0,0 +1,64 @@
+
+// 21.5.1 : important advice about setting the baud rate AFTER transmitter is set
+
+//doc 20.11.2 : TXCn Flag for Transmit Complete interrupt (see description of the TXCIEn bit)
+// doc 20.11.4 : settings of USARTn
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+void	 uart_tx(char c)
+{
+	// doc 20.6.2 example of code
+	// doc 20.6.3 : checks when transmit buffer is empty
+	while (!(UCSR0A & (1<<UDRE0)))
+	{
+	}
+	// doc 20.6.1 : sending frames (5 to 8 bits)
+	UDR0 = c;
+}
+
+void	uart_init()
+{
+	// UART config to 8N1 (8-bit, no parity, stop-bit = 1)
+	// doc 20.6 : enable transmitter 0
+	UCSR0B |= (1 << TXEN0);
+	UCSR0B &= ~(1 << RXEN0);
+
+	// doc 20.11.4 - Table 20-8 : async mode chosen caue asked for "UART" with no S 00
+	UCSR0C &= ~(1 << UMSEL01);
+	UCSR0C &= ~(1 << UMSEL00);
+
+	// doc 20.11.4 - Table 20-9 : parity mode (checks of parity) = none 00
+	UCSR0C &= ~(1 << UPM01);
+	UCSR0C &= ~(1 << UPM00);
+
+	// doc 20.11.4 - Table 20-10 : stop bit select = one (bit set to 0)
+	UCSR0C &= ~(1 << USBS0);
+
+	// doc 20.11.4 - Table 20-11 : character size = 8-bit (011)
+	UCSR0C &= ~(1 << UCSZ02);
+	UCSR0C |= (1 << UCSZ01);
+	UCSR0C |= (1 << UCSZ00);
+
+	// doc 20.11.4 - Table 20-12 : clock plarity for sync mode only, set to 0 for async
+	UCSR0C &= ~(1 << UCPOL0);
+
+	// doc 20.3.1 - Table 20-1 : baudrate or UBRRn calculation
+	// doc 20.11.5 : USART baud rate set with UBRRnH-L
+	// UBRRn = (F_CPU / 8 * BAUD) - 1
+	UBRR0L = (float)((F_CPU / (16.0 * UART_BAUDRATE) + 0.5)) - 1;
+	UBRR0H = 0;
+}
+
+int main()
+{
+	// Figure 20-1 : block diagram of transmission
+
+	uart_init();
+	while (1)
+	{
+		uart_tx('Z');
+		_delay_ms(1000);
+	}
+}
